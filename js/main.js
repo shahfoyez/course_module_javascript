b6f396dcb8d3cb1a91bf124b18fe19ae57fe70e6
shahfoyez <shahfoyez7@gmail.com>
2023-02-07 12:30:49 +0600

    7/2/c1

--- js/main.js
+++ js/main.js
@@ -2,22 +2,23 @@ const milestoneData = JSON.parse(data).data;
 
 function loadMilestones(){
   const milestones = document.querySelector('.milestones');
-  milestones.innerHTML = `<div class="milestone border-b"> 
-        <div class="flex">
-            <div class="checkbox"><input type="checkbox" /></div>
-            <div>
-                <p>
-                    Milestone 1 name
-                    <span><i class="fas fa-chevron-down"></i></span>
-                </p>
+  milestones.innerHTML = `
+    <div class="milestone border-b"> 
+            <div class="flex">
+                <div class="checkbox"><input type="checkbox" /></div>
+                <div>
+                    <p>
+                        Milestone 1 name
+                        <span><i class="fas fa-chevron-down"></i></span>
+                    </p>
+                </div>
+            </div>
+            <div class="hidden_panel">
+                <div class="module border-b">
+                    <p>Module Name</p>
+                </div>
             </div>
         </div>
-        <div class="hidden_panel">
-            <div class="module border-b">
-                <p>Module Name</p>
-            </div>
-        </div>
-    </div>
-  `;
+    `;
 }
 loadMilestones();
