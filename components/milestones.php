7ed26abcea319132df4f66aeb6e51ccf17d23cc3
shahfoyez <shahfoyez7@gmail.com>
2023-02-08 11:28:14 +0600

    8/2/c2

--- components/milestones.php
+++ components/milestones.php
@@ -1,18 +1,3 @@
 <div class="milestones">
-    <!-- <div class="milestone border-b">
-        <div class="flex">
-            <div class="checkbox"><input type="checkbox" /></div>
-            <div>
-                <p>
-                    Milestone 1 name
-                    <span><i class="fas fa-chevron-down"></i></span>
-                </p>
-            </div>
-        </div>
-        <div class="hidden_panel">
-            <div class="module border-b">
-                <p>Module Name</p>
-            </div>
-        </div>
-    </div> -->
+   =
 </div>
--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 const milestoneData = JSON.parse(data).data;
+console.log(milestoneData);
 
 function loadMilestones(){
   const milestones = document.querySelector('.milestones');
-  milestones.innerHTML = `
-    <div class="milestone border-b"> 
+  var hh = milestoneData.join("");
+  console.log(hh);
+
+  milestones.innerHTML = `${milestoneData.map(function(milestone){
+    return `<div class="milestone border-b">
             <div class="flex">
                 <div class="checkbox"><input type="checkbox" /></div>
-                <div>
+                <div onclick="openMilestone(this)">
                     <p>
-                        Milestone 1 name
+                        ${milestone.name}
                         <span><i class="fas fa-chevron-down"></i></span>
                     </p>
                 </div>
             </div>
-            <div class="hidden_panel">
-                <div class="module border-b">
-                    <p>Module Name</p>
-                </div>
+            
+            <div class="hidden_panel show">
+                ${milestone.modules
+                    .map(function(module){
+                        return `<div class="module border-b">
+                            <p>${module.name}</p>
+                        </div>`;
+                })
+                .join("")}  
             </div>
-        </div>
-    `;
+        </div>`;
+  }).join("")}`;
 }
 loadMilestones();
+function openMilestone(milestoneElement ){
+    const currentPanel = milestoneElement.parentNode.nextElementSibling;
+    
+}
