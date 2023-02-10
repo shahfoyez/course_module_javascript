1f28db82741272ca2285df8083e80db386cc25e0
shahfoyez <shahfoyez7@gmail.com>
2023-02-10 15:35:31 +0600

    10/2/c1

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ function loadMilestones(){
   const milestones = document.querySelector('.milestones');
 
   milestones.innerHTML = `${milestoneData.map(function(milestone){
-    return `<div class="milestone border-b">
+    return `<div class="milestone border-b" id="${milestone._id}">
             <div class="flex">
-                <div class="checkbox"><input type="checkbox" /></div>
+                <div class="checkbox"><input type="checkbox" onclick="markMilestone(this, ${milestone._id})"/></div>
                 <div onclick="openMilestone(this, ${milestone._id})">
                     <p>
                         ${milestone.name}
@@ -48,12 +48,44 @@ function openMilestone( milestoneElement, id ){
     showMilestone(id);
 }
 function showMilestone(id){
-    
+    const milestoneImage = document.querySelector('.milestoneImage');
+    const title = document.querySelector('.title');
+    const description = document.querySelector('.details');
+
+    milestoneImage.style.opacity = '0';
+    milestoneImage.src = milestoneData[id].image;
+    title.innerText = milestoneData[id].name;
+    description.innerText = milestoneData[id].description;
+}
+// listen hero image load
+const milestoneImage = document.querySelector('.milestoneImage');
+milestoneImage.onload = function(){
+    this.style.opacity = '1';
+};
+
+function markMilestone(checkbox, id){
+    const doneList = document.querySelector('.doneList');
+    const milestonsList = document.querySelector('.milestones');
+    const item = document.getElementById(id);
+    if(checkbox.checked){
+        // mark as done
+        milestonsList.removeChild(item);
+        doneList.appendChild(item);
+    }else{
+        milestonsList.appendChild(item);
+        doneList.removeChild(item);
+    }
 }
 function foyRemoveBorder(){
     const moduleDiv = document.getElementsByClassName('foy-modules');
+    const milestonesDiv = document.getElementsByClassName('milestones');
+    console.log(milestonesDiv);
+    
     for(var i = 0; i<moduleDiv.length; i++){
         moduleDiv[i].lastElementChild.classList.remove('border-b');
+    }
+    for(var i = 0; i<milestonesDiv.length; i++){
+        milestonesDiv[i].lastElementChild.classList.remove('border-b');
     }
 }
 foyRemoveBorder();
